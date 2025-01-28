832f2b93cd818608ab42455db2e492bdb62c9a5e
mariacsalazar <mcsalazart@gmail.com>
2025-01-29 00:41:29 +0100

    changes to size 460 lambda 0.01

--- config/config.py
+++ config/config.py
@@ -50,4 +50,4 @@ class Config(object):
     momentum=0.9
 
     bias_model_lambda = 0.01
-    num_bias_embedding = 486 # Size of the embedding used for bias prediction
+    num_bias_embedding = 460 # Size of the embedding used for bias prediction
