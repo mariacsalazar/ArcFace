6f31ce727a194abcb7db99d9f8806d7c73895046
mariacsalazar <mcsalazart@gmail.com>
2025-01-31 18:50:48 +0100

    changes to 256 0.02

--- config/config.py
+++ config/config.py
@@ -49,5 +49,5 @@ class Config(object):
     weight_decay = 5e-3
     momentum=0.9
 
-    bias_model_lambda = 0.05
+    bias_model_lambda = 0.02
     num_bias_embedding = 256 # Size of the embedding used for bias prediction
